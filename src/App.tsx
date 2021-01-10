2702a3f888f5b096cdf217ddfc1612847045f32b
Artem Tsybulko <tsybulko.artem@gmail.com>
2021-01-11 01:35:50 +0300

    refactor: canvas

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
 import React from 'react';
 import Canvas from './components/canvas/Canvas';
 
-const App: React.FC = () => {
-  const draw = (ctx: any, frameCount: number): void => {
-    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    ctx.fillStyle = '#000000';
-    ctx.beginPath();
-    ctx.arc(50, 100, 20 * Math.sin(frameCount * 0.05) ** 2, 0, 2 * Math.PI);
-    ctx.fill();
-  };
-
-  return <Canvas draw={draw} />;
+const App: React.FC = (): JSX.Element => {
+  return <Canvas />;
 };
 
 export default App;
--- src/components/canvas/Canvas.tsx
+++ src/components/canvas/Canvas.tsx
@@ -1,18 +1,23 @@
 import React, { useRef, useEffect } from 'react';
-import { DrawType } from '../../types/types';
 
-const Canvas: React.FC<DrawType> = ({ draw }) => {
+const Canvas: React.FC = (): JSX.Element => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  const draw = (ctx: CanvasRenderingContext2D): void => {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.fillStyle = '#000000';
+    ctx.beginPath();
+    ctx.arc(50, 100, 20 * Math.sin(50 * 0.05) ** 2, 0, 2 * Math.PI);
+    ctx.fill();
+  };
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas?.getContext('2d');
-    let frameCount = 0;
     let animationFrameId: number;
 
     const render = () => {
-      frameCount += 1;
-      draw(context, frameCount);
+      if (context) draw(context);
       animationFrameId = window.requestAnimationFrame(render);
     };
     render();
@@ -20,7 +25,7 @@ const Canvas: React.FC<DrawType> = ({ draw }) => {
     return () => {
       window.cancelAnimationFrame(animationFrameId);
     };
-  }, [draw]);
+  }, []);
 
   return <canvas ref={canvasRef} />;
 };
