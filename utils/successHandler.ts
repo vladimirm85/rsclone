a76d7b77714e0004e1dc33831025b7e471793c8e
vladimirm85 <vladimirm85@gmail.com>
2021-01-16 16:29:56 +0200

    fix: successHandler document type

--- utils/successHandler.ts
+++ utils/successHandler.ts
@@ -4,7 +4,7 @@ import { UserDocumentInterface, SaveDocumentInterface } from '../models';
 export const successHandler = (
   res: Response,
   code: number,
-  document: UserDocumentInterface | SaveDocumentInterface | string
+  document: UserDocumentInterface | SaveDocumentInterface | SaveDocumentInterface[] | string
 ): Response =>
   res.status(code).json({
     success: true,
