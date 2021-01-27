b9d18a6ff768856061badc7f8fba1ca65bb96866
vladimirm85 <vladimirm85@gmail.com>
2021-01-28 01:24:07 +0200

    fix: minor fixes

--- app.ts
+++ app.ts
@@ -37,9 +37,8 @@ facebookRouteProtector(passport);
 
 app.use(logger('dev'));
 app.use(cors());
-app.use(express.json({ limit: '10mb' }));
-app.use(express.urlencoded({ limit: '10mb', extended: false }));
-app.use(express.text({ limit: '10mb' }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use('/saves', savesRouter);
 app.use('/auth', authRouter);
