484d8ecd314f2a481037cc65066c56b02c8bea3c
vladimirm85 <vladimirm85@gmail.com>
2021-01-28 23:32:50 +0200

    fix: limit settings

--- app.ts
+++ app.ts
@@ -1,6 +1,5 @@
 import * as express from 'express';
 import * as mongoose from 'mongoose';
-import * as bodyparser from 'body-parser';
 import * as passport from 'passport';
 import * as logger from 'morgan';
 import * as cors from 'cors';
@@ -38,12 +37,8 @@ facebookRouteProtector(passport);
 
 app.use(logger('dev'));
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '200mb' }));
 app.use(express.urlencoded({ extended: false }));
-
-app.use(bodyparser.json({ limit: '20mb' }));
-app.use(bodyparser.urlencoded({ limit: '20mb', extended: true }));
-app.use(bodyparser.text({ limit: '20mb' }));
 
 app.use('/saves', savesRouter);
 app.use('/auth', authRouter);
