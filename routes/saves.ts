bec7cb69f4044beeab084f1f48c9cd3511d74f6c
vladimirm85 <vladimirm85@gmail.com>
2021-01-29 02:11:15 +0200

    refactor: swagger Save refactor 2

--- routes/saves.ts
+++ routes/saves.ts
@@ -4,49 +4,6 @@ import { getAllSaves, getSaveById, createSave, updateSave, deleteSave } from '..
 
 export const savesRouter = Router();
 
-/**
- * @swagger
- * components:
- *   schemas:
- *     Savess:
- *       type: object
- *       properties:
- *         _id:
- *           type: string
- *           description: unique id
- *           example: '60048e26a8486d510406b33f'
- *         saveData:
- *           type: object
- *           description: Data of the saved game
- *           example: 'dgrg'
- *         userId:
- *           type: string
- *           description: User ID
- *           example: '600485e573f5ce001796f558'
- *         createdAt:
- *           type: string
- *           format: date # or date-time
- *           description: When save is created
- *           example: '2021-01-17T19:21:10.492+00:00'
- */
-
-/**
- * @swagger
- * /saves:
- *   get:
- *     tags:
- *       - Saves
- *     description: Returns all saves
- *     responses:
- *       200:
- *         description: An array saves
- *         content:
- *           application/json:
- *             schema:
- *             type: array
- *       500:
- *         description: error
- */
 savesRouter.get('/', passport.authenticate('jwt', { session: false }), getAllSaves);
 
 savesRouter.get('/:id', passport.authenticate('jwt', { session: false }), getSaveById);
