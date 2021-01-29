a1b2287f99391f5f9f3e0a5148ec8e1d34f60cfe
vladimirm85 <vladimirm85@gmail.com>
2021-01-30 00:07:16 +0200

    add: swagger social login routs

--- controllers/auth/loginGoogleController.ts
+++ controllers/auth/loginGoogleController.ts
@@ -1,12 +1,16 @@
 import { Request, Response } from 'express';
 import { pick } from 'lodash';
-import { createToken, TokenUserData } from '../../utils';
+import { createToken, errorHandler, TokenUserData } from '../../utils';
 
 export const loginGoogle = async (req: Request, res: Response): Promise<void | Response> => {
   const user = req.user;
 
-  const token = await createToken(pick(user, ['_id', 'email']) as TokenUserData);
-
   const baseUrl = process.env.FRONT_BASE_URL || 'http://localhost:3000';
+
+  const token = await createToken(pick(user, ['_id', 'email']) as TokenUserData);
+  if (!token) {
+    errorHandler(res, 500, 'login: token was not created');
+  }
+
   res.redirect(`${baseUrl}/social-login-success/?token=${token}`);
 };
