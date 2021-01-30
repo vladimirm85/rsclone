1aa83df6186c5c6e386dc39f108a21d4e1ac1868
onc157 <onc157@yandex.ru>
2021-01-30 21:54:31 +0300

    feat: implement 10 levels

--- src/components/canvas/bgLevelsGradientsData.ts
+++ src/components/canvas/bgLevelsGradientsData.ts
@@ -9,5 +9,37 @@ const bgLevelsGradientsData: LevelsGradients = [
     colorLeft: '#0250c5',
     colorRight: '#d43f8d',
   },
+  {
+    colorLeft: '#fa709a',
+    colorRight: '#fee140',
+  },
+  {
+    colorLeft: '#89f7fe',
+    colorRight: '#66a6ff',
+  },
+  {
+    colorLeft: '#9795f0',
+    colorRight: '#fbc8d4',
+  },
+  {
+    colorLeft: '#92fe9d',
+    colorRight: '#00c9ff',
+  },
+  {
+    colorLeft: '#f83600',
+    colorRight: '#f9d423',
+  },
+  {
+    colorLeft: '#09203f',
+    colorRight: '#537895',
+  },
+  {
+    colorLeft: '#0AD34B',
+    colorRight: '#f9f047',
+  },
+  {
+    colorLeft: '#ffecd2',
+    colorRight: '#fcb69f',
+  },
 ];
 export default bgLevelsGradientsData;
