40990b17af1d4726606dbffe3e6cbb9609a6a157
vladimirm85 <vladimirm85@gmail.com>
2021-01-31 16:13:47 +0200

    fix: fix app import

--- back-end/bin/www.ts
+++ back-end/bin/www.ts
@@ -2,7 +2,7 @@
  * Module dependencies.
  */
 
-import { app } from 'home/vladimir/WebstormProjects/rsclone/back-end/app';
+import { app } from '../app';
 import debug0 from 'debug';
 import * as http from 'http';
 
